5bfbee512c9e9c50deaa61224d07b313cd2c1121
ayushagarwal95 <agarwalayush161@gmail.com>
2015-03-25 22:02:53 +0530

    * Added success.ejs and failiure.ejs * Added routes for success, failiure, cancel

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Gags By Mail' });
 });
 
+router.get('/success', function (req, res) {
+    res.render('success', {});
+});
+
+router.get('/failiure', function (req,res) {
+    res.render('failiure', {});
+});
+
+router.get('/cancel', function (req,res) {
+    res.redirect('index', {title: 'Gags By Mail'});
+});
 
 module.exports = router;
